aeed9b7bb58eecc8494a924e264ff5fbce1d52ed
aduffizi <alessandro.duffizi@lnf.infn.it>
2025-11-28 10:44:53 +0000

    changed unimag db name

--- iocBoot/iocOCEM_E642/OCEM_E642.cmd
+++ iocBoot/iocOCEM_E642/OCEM_E642.cmd
@@ -23,7 +23,7 @@ ocemInit "OCEM_PORT",1,"4"
 # Load database records ## ports name are already define in db
 dbLoadRecords("$(TOP)/db/OCEM_E642.db", "P=DAFNE:MAG:OCEM,R=QUATM05, ADDR=4, IMAX=280,VMAX=40")
 #dbLoadRecords("$(TOP)/db/OCEM_E642.db", "P=BTF:MAG:OCEM,R=SLAVE11, ADDR=11, IMAX=280,VMAX=40")
-dbLoadRecords("$(TOP)/db/unimag-ocem.db", "P=DAFNE:MAG:OCEM,R=QUATM05")
-#dbLoadRecords("$(TOP)/db/unimag-ocem.db", "P=BTF:MAG:OCEM,R=SLAVE10")
+dbLoadRecords("$(TOP)/db/unimag-ocemE642.db", "P=DAFNE:MAG:OCEM,R=QUATM05")
+#dbLoadRecords("$(TOP)/db/unimag-ocemE642.db", "P=BTF:MAG:OCEM,R=SLAVE10")
 
 iocInit()
